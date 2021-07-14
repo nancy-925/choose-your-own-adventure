e4355483daed50edf8f573d20fef6e54ce3348af
nancy-925 <87199719+nancy-925@users.noreply.github.com>
2021-07-14 14:12:45 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 namespace SpriteKind {
     export const Task1 = SpriteKind.create()
     export const Task2 = SpriteKind.create()
+    export const Task3 = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Task1, function (sprite, otherSprite) {
     question1()
@@ -9,9 +10,11 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Task1, function (sprite, otherSp
         welcomeToTheVillage()
         question2A()
         if (task2AnswerA == 1) {
-        	
-        } else if (false) {
-        	
+            decision2A()
+            question3A()
+        } else if (task2AnswerA == 2) {
+            // death
+            decision2B()
         } else {
             game.splash("invalid imput")
         }
@@ -20,9 +23,12 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Task1, function (sprite, otherSp
         welcomeToTheVillage()
         question2B()
         if (task2AnswerB == 1) {
-        	
-        } else if (false) {
-        	
+            decision2C()
+            create_map_2()
+            question3C()
+        } else if (task2AnswerB == 2) {
+            decision2D()
+            question3D()
         } else {
             game.splash("invalid imput")
         }
@@ -30,6 +36,21 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Task1, function (sprite, otherSp
         game.splash("invalid imput")
     }
 })
+function decision2D () {
+    mySprite.say("if i may ask, what are the levels, your majesty?", 5000)
+    pause(5000)
+    villager1.say("w-w-what? how could you say that to the king?", 5000)
+    pause(5000)
+    king.say("hahaha, its ok william. i like this guy's bravery", 5000)
+    pause(5000)
+    king.say("for your information, our town is split into levels ", 5000)
+    pause(5000)
+    king.say("level 1 being the wealthiest, and level 6 being the poorest", 5000)
+    pause(5000)
+    villager1.say("most newcomers stay in level 4", 3000)
+    pause(3000)
+    mySprite.say("i see, thank you, your majesty")
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
         . . . . . . f f f f . . . . . . 
@@ -50,6 +71,19 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . f f . . f f . . . . . 
         `)
 })
+function question3A () {
+    king.say("hmm that is up to him", 3000)
+    pause(3000)
+    game.showLongText("you know have two options", DialogLayout.Bottom)
+    game.showLongText("press 1 for \"yes i would like to stay in the village", DialogLayout.Bottom)
+    game.showLongText("press 2 for \"thank you for the kind offer, but i'll pass.", DialogLayout.Bottom)
+    task3AnswerA = parseFloat(game.askForString("What is your choice?"))
+}
+function create_map_2 () {
+    tiles.setTilemap(tilemap`level2`)
+    king.setFlag(SpriteFlag.Invisible, true)
+    villager2.setFlag(SpriteFlag.Invisible, false)
+}
 function create_player () {
     mySprite = sprites.create(img`
         . . . . . . f f f f . . . . . . 
@@ -72,8 +106,8 @@ function create_player () {
     controller.moveSprite(mySprite)
     scene.cameraFollowSprite(mySprite)
 }
-function task_one () {
-    task1 = sprites.create(img`
+function villager () {
+    villager1 = sprites.create(img`
         . . . . f f f f . . . . . 
         . . f f f f f f f f . . . 
         . f f f f f f c f f f . . 
@@ -91,35 +125,22 @@ function task_one () {
         . . . f f f f f f . . . . 
         . . . f f . . f f . . . . 
         `, SpriteKind.Task1)
-    task1.setPosition(112, 61)
+    villager1.setPosition(112, 61)
 }
 function question2A () {
-    task1.say("My king, I found this wanderer on the outskirts of town.", 6000)
-    pause(5000)
-    task1.say("He might be the one who destroyed the crops!", 5000)
-    pause(3000)
-    task2.say("hmmm, what do you have to say for yourself?", 5000)
-    pause(3000)
-    game.splash("you now have two options")
-    game.splash("press 1 for \"Your majesty,", " I did no such thing!")
-    game.splash("press 2 for \"This ", "questioning is quite unnecessary, can I please get settled down?")
+    villager1.say("My king, I found this wanderer on the outskirts of town.", 7000)
+    pause(7000)
+    villager1.say("He might be the one who destroyed the crops!", 6000)
+    pause(6000)
+    king.say("hmmm, what do you have to say for yourself?", 6000)
+    pause(6000)
+    game.showLongText("you now have two options", DialogLayout.Bottom)
+    game.showLongText("press 1 for \"Your majesty, I did no such thing!", DialogLayout.Bottom)
+    game.showLongText("press 2 for \"This questioning is quite unnecessary, can I please get settled down?", DialogLayout.Bottom)
     task2AnswerA = parseFloat(game.askForString("What is your choice?"))
 }
-function question1 () {
-    task1.say("HEY YOU", 3000)
-    pause(3000)
-    task1.say("who are you and what are you doing here?", 5000)
-    pause(5000)
-    task1.say("are you the one who destroyed the crops?", 5000)
-    pause(5000)
-    task1.say("")
-    game.splash("you now have two options")
-    game.splash("press 1 for \"sir i don't ", "know what you're talking about, i just stumbled across this town.\"")
-    game.splash("press 2 for \"who are you? ", "you have no right to talk to me like that, im just minding my own business.\"")
-    task1Answer = parseFloat(game.askForString("What is your choice?"))
-}
-function task_two () {
-    task2 = sprites.create(img`
+function theKing () {
+    king = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . 5 5 . . . 5 5 . . . 5 5 . . 
         . . . 5 5 . . 5 5 . . 5 5 . . . 
@@ -137,7 +158,20 @@ function task_two () {
         . . . . . f f f f f f . . . . . 
         . . . . . f f . . f f . . . . . 
         `, SpriteKind.Task2)
-    task2.setPosition(450, 75)
+    king.setPosition(450, 75)
+}
+function question1 () {
+    villager1.say("HEY YOU", 3000)
+    pause(3000)
+    villager1.say("who are you and what are you doing here?", 5000)
+    pause(5000)
+    villager1.say("are you the one who destroyed the crops?", 5000)
+    pause(5000)
+    villager1.say("")
+    game.showLongText("you now have two options", DialogLayout.Bottom)
+    game.showLongText("press 1 for \"sir i don't know what you're talking about, i just stumbled across this town.\"", DialogLayout.Bottom)
+    game.showLongText("press 2 for \"who are you? you have no right to talk to me like that, im just minding my own business.\"", DialogLayout.Bottom)
+    task1Answer = parseFloat(game.askForString("What is your choice?"))
 }
 function intro () {
     game.splash("Welcome to Choose Your ", "Own Adventure!")
@@ -168,20 +202,36 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 function decision1B () {
     mySprite.say("who are you? you have no right to talk to me like that ", 8000)
     pause(8000)
-    task1.say("you're right, my dearest apologises", 5000)
+    villager1.say("you're right, my dearest apologises", 5000)
     pause(5000)
-    task1.say("let me take you to my king, you can stay with us at our village.", 8000)
+    villager1.say("let me take you to my king, you can stay with us at our village.", 8000)
     pause(8000)
 }
 function welcomeToTheVillage () {
-    task1.vx += 35
+    villager1.vx += 35
     pause(5000)
-    task1.vx += -35
-    task1.say("this is our village, my king is inside.", 5000)
+    villager1.vx += -35
+    villager1.say("this is our village, my king is inside.", 5000)
     pause(5000)
-    task1.vx += 35
+    villager1.vx += 35
     pause(5000)
-    task1.vx += -35
+    villager1.vx += -35
+}
+function decision2C () {
+    mySprite.say("thank you, your majesty", 3000)
+    pause(3000)
+    king.say("william, show this man to his new home", 5000)
+    pause(5000)
+    villager1.say("yes, your majesty", 2000)
+    pause(2000)
+    villager1.say("follow me", 1000)
+    pause(1000)
+    villager1.vx += -35
+    pause(3500)
+    villager1.vx += 35
+    villager1.vy += 35
+    pause(2000)
+    villager1.vy = 0
 }
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
@@ -203,6 +253,23 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . f f . . . f f f . . . . 
         `)
 })
+function question3C () {
+    villager1.say("now, we are in the 5th level", 3000)
+    pause(3000)
+    villager1.say("in front of us, there is the 4th level", 4000)
+    pause(4000)
+    villager1.say("and in front of that is the 3rd level", 4000)
+    pause(4000)
+    mySprite.say("what about the 6th level?", 3000)
+    pause(3000)
+    villager1.say("the 6th level is on a island far away. you don't want to end up there", 7000)
+    pause(7000)
+    villager2.vx += -50
+    pause(2000)
+    villager2.vx += 50
+    villager2.say("hello kind sir! do you have any food?", 4000)
+    pause(4000)
+}
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
         . . . . . . f f f f . . . . . . 
@@ -223,39 +290,87 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . f f . . f f . . . . . 
         `)
 })
+function question3D () {
+    king.say("you know, you seem like the perfect person i should add to my personal army", 10000)
+    pause(10000)
+    king.say("straightforward, but not rude", 3000)
+    pause(3000)
+    king.say("brave, but not stupid", 2000)
+    pause(2000)
+    king.say("would you like to join my personal army?", 4000)
+    pause(4000)
+    task3AnswerD = parseFloat(game.askForString("What is your choice?"))
+    game.showLongText("you now have two options", DialogLayout.Bottom)
+    game.showLongText("press 1 for \"that sounds wonderful, yes, I would like to join", DialogLayout.Bottom)
+    game.showLongText("press 2 for \"thank you, your majesty for the offer, but I'll pass\"", DialogLayout.Bottom)
+}
 function decision1A () {
     mySprite.say("sir i don't know what you're talking about, i just stumbled across this town.", 10000)
     pause(10000)
-    task1.say("hmmm, fine then, let me take you to my king to see if he recognizes you", 8000)
+    villager1.say("hmmm, fine then, let me take you to my king to see if he recognizes you", 8000)
     pause(8000)
 }
 function decision2A () {
-    task2.say("hmm i think he seems like the trustworthy type", 4000)
+    king.say("hmm i think he seems like the trustworthy type", 4000)
     pause(4000)
-    task2.say("i believe him", 2000)
+    king.say("i believe him", 2000)
     pause(2000)
-    task1.say("very well, then should i have him stay in the village?")
+    villager1.say("very well, then should i have him stay in the village?", 5000)
+    pause(5000)
 }
 function create_map () {
     tiles.setTilemap(tilemap`level1`)
 }
+function villager3 () {
+    villager2 = sprites.create(img`
+        . f f f . f f f f . f f f . 
+        f f f f f c c c c f f f f f 
+        f f f f b c c c c b f f f f 
+        f f f c 3 c c c c 3 c f f f 
+        . f 3 3 c c c c c c 3 3 f . 
+        . f c c c c 4 4 c c c c f . 
+        . f f c c 4 4 4 4 c c f f . 
+        . f f f b f 4 4 f b f f f . 
+        . f f 4 1 f d d f 1 4 f f . 
+        . . f f d d d d d d f f . . 
+        . . e f e 4 4 4 4 e f e . . 
+        . e 4 f b 3 3 3 3 b f 4 e . 
+        . 4 d f 3 3 3 3 3 3 c d 4 . 
+        . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+        . . . . f f f f f f . . . . 
+        . . . . f f . . f f . . . . 
+        `, SpriteKind.Task3)
+    villager2.setPosition(460, 40)
+    villager2.setFlag(SpriteFlag.Invisible, true)
+}
 function question2B () {
-    task1.say("Your Majesty, this sir needs a place to stay.", 4000)
-    pause(4000)
-    task1.say("Perhaps the 5th ring?", 2000)
+    villager1.say("Your Majesty, this sir needs a place to stay.", 5000)
+    pause(5000)
+    villager1.say("Perhaps the 5th ring?", 2000)
     pause(2000)
-    task2.say("Have a guest stay in the 5th ring? No, that would be quite rude of us.", 8000)
+    king.say("Have a guest stay in the 5th ring? No, that would be quite rude of us.", 8000)
     pause(8000)
-    task2.say("You can stay in the 3rd ring", 2000)
-    pause(2000)
-    game.splash("you now have two ", "options")
-    game.splash("press 1 for \"thank you,", "your majesty\"")
-    // can you make more rows of text?
-    game.splash("press 2 for \"if i may ask,", "what are the rings, your majesty?\"")
+    king.say("You can stay in the 3rd ring", 3000)
+    pause(3000)
+    game.showLongText("you now have two options ", DialogLayout.Bottom)
+    game.showLongText("press 1 for \"thank you, your majesty", DialogLayout.Bottom)
+    game.showLongText("press 2 for \"if i may ask, what are the rings, your majesty?\"", DialogLayout.Bottom)
     task2AnswerB = parseFloat(game.askForString("What is your choice?"))
 }
-let task2: Sprite = null
-let task1: Sprite = null
+function decision2B () {
+    king.say("WHAT", 500)
+    pause(500)
+    king.say("YOU DARE SAY THAT TO ME?!?!?", 3000)
+    pause(3000)
+    king.say("EXECUTE HIM IMMEDIATELY", 2000)
+    pause(2000)
+    game.over(false, effects.splatter)
+}
+let task3AnswerD = 0
+let villager2: Sprite = null
+let task3AnswerA = 0
+let king: Sprite = null
+let villager1: Sprite = null
 let mySprite: Sprite = null
 let task2AnswerB = 0
 let task2AnswerA = 0
@@ -263,5 +378,6 @@ let task1Answer = 0
 create_map()
 intro()
 create_player()
-task_one()
-task_two()
+villager()
+theKing()
+villager3()
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -7,24 +7,65 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile6 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile9 = image.ofBuffer(hex``);
+    export const tile10 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile7 = image.ofBuffer(hex``);
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile20 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile21 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile8 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
+    export const tile9 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile22 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile23 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile24 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`200008000a0a0a0a0a0a0a0a0c09090909091112090d0909090e090909090b0b0b09090d0a0a0a0a0a07050a0c09090909090f1009090909090e090b0d090909090d090b0a0a0a0a0a08060a0c0909090909090d090d0d09090e0909090d090d090909090a0a0a0a0a0a0a0a0c090909090d0909090909090d0e090d090b0909030409090a0a0a0a0a0a0a0a0c09090911120909090d1112090e0b0d09090b0b0102090d0a07050a0a0a0a0a0c0909090f10090d090d0f10090e09090909090909090d090a08060a0a0a0a0a0c0909090909090909090909090e0b09030409090d09090b0a0a0a0a0a0a0a0a0c09090909090d0909090909090e09090102090b09090d09`, img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile15,myTiles.tile9,myTiles.tile7,myTiles.tile8,myTiles.tile4,sprites.dungeon.floorLight2,sprites.dungeon.floorDark2,sprites.dungeon.floorLight3,sprites.dungeon.floorMixed,sprites.dungeon.floorLightMoss,sprites.dungeon.floorLight5,myTiles.tile18,myTiles.tile19,myTiles.tile20,myTiles.tile21], TileScale.Sixteen);
             case "level1":
-            case "level1":return tiles.createTilemap(hex`20000800040b040404040b0b0b0404040404040412161616161616161616161616161616020101010101030202020201010302021214151415141514151415141514151401030202020201010101030302020201121313131313131313131313131313130809090909090909090909090909090912111111111111111111111111111111070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a1211110f0c1111111111111111111111070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a1211110d0e1111110f0c111111111111070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a12111111111111110d0e1111111111110506060606060606060606060606060612111111111111111111111111111111`, img`
+            case "level1":return tiles.createTilemap(hex`20000800040b040404040b0b0b040404040404040e121212121212121212121212121212020101010101030202020201010302020e101110111011101110111011101110010302020202010101010303020202010e0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f080909090909090909090909090909090e0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0e0d0d150c0d0d0d0d0d0d0d0d0d0d0d070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0e0d0d18170d0d0d150c0d0d0d0d0d0d070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0e0d0d0d0d0d0d0d18170d0d0d0d0d0d050606060606060606060606060606060e0d0d0d13140d0d0d0d0d0d0d0d0d0d`, img`
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -33,7 +74,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tileGrass3,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.builtin.forestTiles0,sprites.castle.tilePath7,sprites.castle.tilePath8,sprites.castle.tilePath4,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath5,sprites.swamp.swampTile0,myTiles.tile3,myTiles.tile4,myTiles.tile7,myTiles.tile8,myTiles.tile9,sprites.dungeon.floorLight2,sprites.dungeon.floorLight0,sprites.dungeon.chestClosed,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.stairEast], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.castle.tileGrass3,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.builtin.forestTiles0,sprites.castle.tilePath7,sprites.castle.tilePath8,sprites.castle.tilePath4,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath5,sprites.swamp.swampTile0,myTiles.tile3,sprites.dungeon.floorLight2,sprites.dungeon.floorLight0,sprites.dungeon.chestClosed,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.stairEast,myTiles.tile10,myTiles.tile11,myTiles.tile2,myTiles.tile22,myTiles.tile23,myTiles.tile24], TileScale.Sixteen);
         }
         return null;
     })
@@ -45,20 +86,50 @@ namespace myTiles {
             case "tile5":return tile5;
             case "myTile":
             case "tile1":return tile1;
-            case "myTile0":
-            case "tile2":return tile2;
             case "myTile4":
             case "tile6":return tile6;
-            case "myTile7":
-            case "tile9":return tile9;
-            case "myTile5":
-            case "tile7":return tile7;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile14":
+            case "tile16":return tile16;
+            case "myTile15":
+            case "tile17":return tile17;
+            case "myTile16":
+            case "tile18":return tile18;
+            case "myTile17":
+            case "tile19":return tile19;
+            case "myTile18":
+            case "tile20":return tile20;
+            case "myTile19":
+            case "tile21":return tile21;
             case "myTile6":
             case "tile8":return tile8;
-            case "myTile1":
-            case "tile3":return tile3;
+            case "myTile7":
+            case "tile9":return tile9;
             case "myTile2":
             case "tile4":return tile4;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile13":
+            case "tile15":return tile15;
+            case "myTile20":
+            case "tile22":return tile22;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile21":
+            case "tile23":return tile23;
+            case "myTile22":
+            case "tile24":return tile24;
         }
         return null;
     })
