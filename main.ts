b2af08607c4827cef2acad0fb88a81d3c3c402b0
nancy-925 <87199719+nancy-925@users.noreply.github.com>
2021-07-16 12:46:58 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ namespace SpriteKind {
     export const Task1 = SpriteKind.create()
     export const Task2 = SpriteKind.create()
     export const Task3 = SpriteKind.create()
+    export const Taco = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Task1, function (sprite, otherSprite) {
     question1()
@@ -13,9 +14,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Task1, function (sprite, otherSp
             decision2A()
             question3A()
             if (task3AnswerA == 1) {
-            	
+                decision3A()
             } else if (task3AnswerA == 2) {
-            	
+                decision3B()
             } else {
                 game.splash("invalid input")
             }
@@ -30,14 +31,13 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Task1, function (sprite, otherSp
         welcomeToTheVillage()
         question2B()
         if (task2AnswerB == 1) {
-            let task3AnswerC = 0
             decision2C()
             create_map_2()
             question3C()
             if (task3AnswerC == 1) {
-            	
+                decision3C()
             } else if (task3AnswerC == 2) {
-            	
+                decision3D()
             } else {
                 game.splash("invalid input")
             }
@@ -45,9 +45,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Task1, function (sprite, otherSp
             decision2D()
             question3D()
             if (task3AnswerD == 1) {
-            	
+                decision3E()
             } else if (task3AnswerD == 2) {
-            	
+                decision3F()
             } else {
                 game.splash("invalid input")
             }
@@ -57,6 +57,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Task1, function (sprite, otherSp
     } else {
         game.splash("invalid input")
     }
+})
+sprites.onOverlap(SpriteKind.Task3, SpriteKind.Taco, function (sprite, otherSprite) {
+    otherSprite.setFlag(SpriteFlag.Invisible, true)
 })
 function decision2D () {
     mySprite.say("if i may ask, what are the levels, your majesty?", 5000)
@@ -71,7 +74,8 @@ function decision2D () {
     pause(5000)
     villager1.say("most newcomers stay in level 4", 3000)
     pause(3000)
-    mySprite.say("i see, thank you, your majesty")
+    mySprite.say("i see, thank you, your majesty", 3000)
+    pause(3000)
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
@@ -185,14 +189,14 @@ function theKing () {
 function question1 () {
     villager1.say("HEY YOU", 3000)
     pause(3000)
-    villager1.say("who are you and what are you doing here?", 5000)
+    villager1.say("who are you? do you even live here?", 5000)
     pause(5000)
     villager1.say("are you the one who destroyed the crops?", 5000)
     pause(5000)
     villager1.say("")
     game.showLongText("you now have two options", DialogLayout.Bottom)
     game.showLongText("press 1 for \"sir i don't know what you're talking about, i just stumbled across this town.\"", DialogLayout.Bottom)
-    game.showLongText("press 2 for \"who are you? you have no right to talk to me like that, im just minding my own business.\"", DialogLayout.Bottom)
+    game.showLongText("press 2 for \"who are you? im just minding my own business.\"", DialogLayout.Bottom)
     task1Answer = parseFloat(game.askForString("What is your choice?"))
 }
 function intro () {
@@ -221,10 +225,21 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . f f f . . . f f . . . . . 
         `)
 })
+function decision3F () {
+    mySprite.say("thank you, your majesty for the offer, but I'll pass", 6000)
+    pause(6000)
+    king.say("*raises eyebrows* very well", 3000)
+    pause(3000)
+    king.say("william, you know what you have to do", 4000)
+    pause(4000)
+    game.showLongText("the king orders william to kill you for refusing the offer \\n \\n number one rule: don't piss the king off", DialogLayout.Full)
+    game.showLongText("oh well, you can always try again :D", DialogLayout.Full)
+    game.over(false)
+}
 function decision1B () {
-    mySprite.say("who are you? you have no right to talk to me like that ", 8000)
+    mySprite.say("who are you? im just minding my own business", 8000)
     pause(8000)
-    villager1.say("you're right, my dearest apologises", 5000)
+    villager1.say("you're right, my *dearest* apologies", 5000)
     pause(5000)
     villager1.say("let me take you to my king, you can stay with us at our village.", 8000)
     pause(8000)
@@ -291,6 +306,10 @@ function question3C () {
     villager2.vx += 50
     villager2.say("hello kind sir! do you have any food?", 4000)
     pause(4000)
+    game.showLongText("You now have two options.", DialogLayout.Bottom)
+    game.showLongText("press 1 for \"um yes, here's a taco\"", DialogLayout.Bottom)
+    game.showLongText("press 2 for \"no i don't\" *death glare*", DialogLayout.Bottom)
+    task3AnswerC = parseFloat(game.askForString("What is your choice?"))
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
@@ -312,6 +331,19 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . f f . . f f . . . . . 
         `)
 })
+function decision3E () {
+    mySprite.say("that sounds wonderful, yes, I would like to join", 5000)
+    pause(5000)
+    king.say("wonderful, then i'll have you get started right away!", 6000)
+    pause(6000)
+    villager1.say("hmpf *glare*", 1000)
+    pause(1000)
+    game.showLongText("you now have a high status, as you are part of the king's personal army. you are showered in riches and are living your best life.", DialogLayout.Full)
+    game.showLongText("however, the security guard is quite jealous of you. so, one day he murders you in your sleep", DialogLayout.Full)
+    game.showLongText("you lived a short but great life", DialogLayout.Full)
+    game.showLongText("(honestly i dont know if this counts as a win or a lose...)", DialogLayout.Full)
+    game.over(true)
+}
 function question3D () {
     king.say("you know, you seem like the perfect person i should add to my personal army", 10000)
     pause(10000)
@@ -321,10 +353,10 @@ function question3D () {
     pause(2000)
     king.say("would you like to join my personal army?", 4000)
     pause(4000)
-    task3AnswerD = parseFloat(game.askForString("What is your choice?"))
     game.showLongText("you now have two options", DialogLayout.Bottom)
     game.showLongText("press 1 for \"that sounds wonderful, yes, I would like to join", DialogLayout.Bottom)
     game.showLongText("press 2 for \"thank you, your majesty for the offer, but I'll pass\"", DialogLayout.Bottom)
+    task3AnswerD = parseFloat(game.askForString("What is your choice?"))
 }
 function decision1A () {
     mySprite.say("sir i don't know what you're talking about, i just stumbled across this town.", 10000)
@@ -344,7 +376,19 @@ function create_map () {
     tiles.setTilemap(tilemap`level1`)
 }
 function decision3B () {
-    mySprite.say("press 2 for \"thank you for the kind offer, but i'll pass.")
+    mySprite.say("thank you for the kind offer, but i'll pass")
+    king.say("fine, william please show this young man out")
+    game.showLongText("you leave the village and try to survive on your own. you also try scouting for other villages.", DialogLayout.Full)
+    game.showLongText("however, one day you get attacked by a wild creature and die from your injuries.", DialogLayout.Full)
+    game.over(false)
+}
+function decision3A () {
+    mySprite.say("yes i would like to stay in the village", 4000)
+    pause(4000)
+    game.showLongText("congrats! you have finished this adventure. you now live a peaceful and happy life at the village.", DialogLayout.Full)
+    game.showLongText("you are on good terms with everyone, and have many friends at the village. Yyu eventually die of old age.", DialogLayout.Full)
+    game.showLongText("a bit boring? you can always go back and try some riskier routes", DialogLayout.Full)
+    game.over(true)
 }
 function villager3 () {
     villager2 = sprites.create(img`
@@ -389,13 +433,58 @@ function decision2B () {
     pause(3000)
     king.say("EXECUTE HIM IMMEDIATELY", 2000)
     pause(2000)
+    game.showLongText("umm yea pissing off the king isn't a good idea unless you have a death wish", DialogLayout.Full)
+    game.showLongText("its ok you can always try again :)", DialogLayout.Full)
     game.over(false, effects.splatter)
 }
+function decision3D () {
+    mySprite.say("no i don't\" *death glare*")
+    villager2.say("oh...ok....")
+    mySprite.vx += -40
+    game.showLongText("wow you managed to piss off the security guard and the villagers. nice going", DialogLayout.Full)
+    game.showLongText("everyone at the village hates you and you live a lonely and miserable life", DialogLayout.Full)
+    game.showLongText("try to make some friends next time ;)", DialogLayout.Full)
+    game.over(false)
+}
+function decision3C () {
+    mySprite.say("um yes, here's a taco", 3000)
+    pause(3000)
+    taco = sprites.createProjectileFromSprite(img`
+        . . . . . . . e e e e . . . . . 
+        . . . . . e e 4 5 5 5 e e . . . 
+        . . . . e 4 5 6 2 2 7 6 6 e . . 
+        . . . e 5 6 6 7 2 2 6 4 4 4 e . 
+        . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
+        . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
+        . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
+        e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
+        e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
+        e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
+        e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
+        e 5 e c 5 4 5 4 5 5 5 e e . . . 
+        e 5 e e 5 5 5 5 5 4 e . . . . . 
+        4 5 4 e 5 5 5 5 e e . . . . . . 
+        . 4 5 4 5 5 4 e . . . . . . . . 
+        . . 4 4 e e e . . . . . . . . . 
+        `, mySprite, 0, -50)
+    taco.setKind(SpriteKind.Taco)
+    taco.follow(villager2, 50)
+    villager2.say("thank you very much!", 3000)
+    pause(3000)
+    villager2.say("us villagers will remember your kindness!", 5000)
+    pause(5000)
+    game.showLongText("the villagers all like you, but the security guard (william) doesn't", DialogLayout.Full)
+    game.showLongText("so, you live in the third level your entire life and have no chance of leveling up because of william.", DialogLayout.Full)
+    game.showLongText("your situation could be worse (pstt check out some of the other routes), so i suppose you won.", DialogLayout.Full)
+    game.over(true)
+}
+let taco: Sprite = null
 let villager2: Sprite = null
 let king: Sprite = null
 let villager1: Sprite = null
 let mySprite: Sprite = null
 let task3AnswerD = 0
+let task3AnswerC = 0
 let task2AnswerB = 0
 let task3AnswerA = 0
 let task2AnswerA = 0
